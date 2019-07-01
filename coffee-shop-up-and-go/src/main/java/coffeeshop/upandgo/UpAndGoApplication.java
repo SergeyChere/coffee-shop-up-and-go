eee60b44050d57a6afe1e3c4c9f890a758a3a39e
Admin <che.sergii@gmail.com>
2019-07-01 16:17:56 +0300

    third commit

---
+++ coffee-shop-up-and-go/src/main/java/coffeeshop/upandgo/UpAndGoApplication.java
@@ -0,0 +1,12 @@
+package coffeeshop.upandgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpAndGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpAndGoApplication.class, args);
+    }
+}
